267407814d1d09859e32d503100dc888795c6626
Arib Dhuka <atxarib99@gmail.com>
2018-09-28 22:31:21 -0500

    Class that runs the Python script file
    Runs the script file and outputs it BufferedReader to be processed.

---
+++ src/pitsoftware/PythonComm.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pitsoftware;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Arib
+ */
+public class PythonComm {
+    
+    public static BufferedReader run() throws IOException {
+        
+        Process process = Runtime.getRuntime().exec("python /Users/aribdhuka/Documents/FSAE/PitSoftware/serialdata.py");
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader errors = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        
+        String line;
+        while((line = errors.readLine()) != null) {
+            System.out.println("Error: " + line);
+        }
+        
+        return reader;
+    }
+}
