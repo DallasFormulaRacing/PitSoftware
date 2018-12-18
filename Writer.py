2fd032de1e05517fa6c645830d75d49ef95bf2c9
Arib Dhuka <atxarib99@gmail.com>
2018-12-17 23:37:34 -0600

    New python file that writes the data to a file instead of returning

---
+++ Writer.py
@@ -0,0 +1,29 @@
+import time
+import os
+import sys
+sys.path.append("/anaconda3/lib/python3.6/site-packages")
+import serial
+import time
+
+ser = serial.Serial('/dev/cu.usbmodem144401', 115200)
+
+input = ser.readline()
+stringInput = input.decode("utf-8")
+
+if(os.path.exists("data.txt")):
+    os.remove("data.txt")
+f = open("data.txt", "w+")
+
+counter = 1
+while(True):
+    if stringInput[0] == '#':
+        f.write(stringInput + "\n")
+    else:
+        print("nothing")
+    input = ser.readline()
+    stringInput = input.decode("utf-8")
+    counter+=1
+    if counter % 10 == 0:
+        f.close()
+        time.sleep(.01)
+        f = open("data.txt", "a")
