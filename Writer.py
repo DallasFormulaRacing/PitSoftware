a6d6aadf2be4f2e646fe2b53895bb7cd9b6ffb32
Arib Dhuka <atxarib99@gmail.com>
2019-02-03 13:32:13 -0600

    Tried to debug python
    Tried to debug the python script but it was a problem with the xBee, not the script.

--- Writer.py
+++ Writer.py
@@ -4,16 +4,20 @@ import sys
 import serial
 import time
 
-ser = serial.Serial('/dev/cu.usbmodem144401', 115200)
-
-input = ser.readline()
-stringInput = input.decode("utf-8")
-
+print("trying to create Serial")
+ser = serial.Serial('/dev/cu.usbmodem143401', 115200)
+print("created")
+print("read input")
 if(os.path.exists("data.txt")):
     os.remove("data.txt")
+print("removed file if existed")
 f = open("data.txt", "w+")
-
+f.write("hello")
+print("Created and wrote")
+input = ser.readline()
+stringInput = input.decode("utf-8")
 counter = 1
+print("About to enter")
 while(True):
     if stringInput[0] == '#':
         f.write(stringInput + "\n")
